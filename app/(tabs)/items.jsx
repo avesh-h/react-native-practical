51b6abcae3f8999cb9587b53abf173af428671ac
Avesh <aveshhasanfatta1155@gmail.com>
2025-02-07 18:22:26 +0530

    files

---
+++ app/(tabs)/items.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+const Items = () => {
+  return (
+    <View>
+      <Text>Items</Text>
+    </View>
+  );
+};
+
+export default Items;

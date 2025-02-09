3203dc7398b309065a8da65c1659568f241ac376
Avesh <aveshhasanfatta1155@gmail.com>
2025-02-09 20:43:48 +0530

    address form fields

--- app/manual-address.tsx
+++ app/manual-address.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { FormProvider, useForm } from "react-hook-form";
 import * as yup from "yup";
 
 import { yupResolver } from "@hookform/resolvers/yup";
 import RHFTextField from "@/components/hook-form/RHFTextField";
+import { Checkbox } from "react-native-paper";
 
 const AddressSchema = yup.object().shape({
   pincode: yup.string().required("Pincode is required"),
@@ -27,12 +34,98 @@ function ManualAdress() {
 
   //create this page according to figma design using RHFTextField
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <FormProvider {...methods}>
-        <RHFTextField name="pincode" placeholder="Pincode" />
-        <RHFTextField name="city" placeholder="City" />
+        <View>
+          <Text style={styles.sectionTitle}>Address</Text>
+          <RHFTextField name="pincode" placeholder="Pincode" />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "100%",
+              gap: 10,
+              paddingTop: 12,
+            }}
+          >
+            <RHFTextField
+              name="city"
+              placeholder="City"
+              fieldStyle={{ width: "50%" }}
+            />
+            <RHFTextField
+              name="state"
+              placeholder="State"
+              fieldStyle={{ width: "50%" }}
+            />
+          </View>
+          <RHFTextField
+            name="flatNo"
+            placeholder="House/Flat no"
+            fieldStyle={{ marginTop: 10 }}
+          />
+          <RHFTextField
+            name="buildingNo"
+            placeholder="Building no"
+            fieldStyle={{ marginTop: 10 }}
+          />
+          <RHFTextField
+            name="buildingNo"
+            multiline={true}
+            numberOfLines={16}
+            placeholder="Road Name/ Area / Colony"
+            fieldStyle={{ marginTop: 10, padding: 12 }}
+          />
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.sectionTitle}>Receiver's details</Text>
+          <RHFTextField
+            name="receiverName"
+            placeholder="Receiver's name"
+            fieldStyle={{ marginTop: 12 }}
+          />
+          <RHFTextField
+            name="receiverPhone"
+            placeholder="Receiver's phone number"
+            fieldStyle={{ marginTop: 12 }}
+          />
+          <RHFTextField
+            name="petName"
+            placeholder="Pet's name"
+            fieldStyle={{ marginTop: 12 }}
+          />
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Checkbox
+            status={"checked"}
+            color="black"
+            onPress={() => {
+              // setChecked(!checked);
+            }}
+          />
+          <Text>Set as default address</Text>
+          <TouchableOpacity
+            onPress={
+              () => {}
+              // setAddress((prev) => ({
+              //   ...prev,
+              //   isDefaultAddress: !prev.isDefaultAddress,
+              // }))
+            }
+          ></TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.saveButton}>
+          <Text style={styles.saveButtonText}>Save Address</Text>
+        </TouchableOpacity>
       </FormProvider>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -42,6 +135,24 @@ const styles = StyleSheet.create({
     paddingTop: 18,
     flex: 1,
     backgroundColor: "#F5F6FB",
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: "500",
+    marginBottom: 15,
+    color: "#333",
+  },
+  saveButton: {
+    backgroundColor: "#EF6C00",
+    padding: 15,
+    borderRadius: 5,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  saveButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
 

f3ca172a613ce4fb819f308807255ad4b6a630cd
Avesh <aveshhasanfatta1155@gmail.com>
2025-02-09 22:56:16 +0530

    added the current location flow

--- components/LocationEnabler.tsx
+++ components/LocationEnabler.tsx
@@ -3,12 +3,16 @@ import { StyleSheet, View, Text } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import StyledButton from "./Button";
 
-function LocationEnabler() {
+type Props = { title1?: string; customStyles?: object };
+
+function LocationEnabler({ title1, customStyles }: Props) {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, customStyles]}>
       <MaterialIcons name="location-off" size={26} color="black" />
       <View style={styles.description}>
-        <Text style={styles.text1}>Enable location permission</Text>
+        <Text style={styles.text1}>
+          {title1 ? title1 : "Enable location permission"}
+        </Text>
         <Text style={styles.text2}>
           Your precise location helps us deliver on time
         </Text>
